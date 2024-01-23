2ff8a17dc27dbd9786ac857a9ba62a94f2b668b1
Georg Schwab <georg.schwab@xentara.io>
2024-01-23 12:53:19 +0100

    Cosmetic changes to specialization of std::is_error_code_enum

--- src/CustomError.hpp
+++ src/CustomError.hpp
@@ -32,14 +32,8 @@ inline auto make_error_code(CustomError error) noexcept -> std::error_code
 
 } // namespace xentara::plugins::templateDriver
 
-/// @brief Template specialization for use with the standard C++ library
-namespace std
-{
-
 /// @brief This specialization enables automatic conversion from xentara::plugins::templateDriver::CustomError to std::error_code.
 template<>
-struct is_error_code_enum<xentara::plugins::templateDriver::CustomError> : public std::true_type
+struct std::is_error_code_enum<xentara::plugins::templateDriver::CustomError> : public std::true_type
 {
 };
-
-} // namespace std
